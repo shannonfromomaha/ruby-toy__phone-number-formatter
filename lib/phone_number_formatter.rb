c8670de7501e018b468e3e374bf02dc6824a0369
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-25 14:05:49 -0600

    phonenumber_done

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,6 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  
+  number = phone_number_str.to_s.gsub(/[[:punct:]]/, "").gsub(/\s+/, "").to_s.split("")
+  return "(" + number[0..2].join + ") " + number[3..5].join + "-" + number[6..9].join 
 end
